d0fff468d4fdc86a0b3b2694b3296561cc9376b0
Benachir_Mohammed_Amine <aminebenachir95@gmail.com>
2023-01-06 00:29:23 +0000

    added

--- README.md
+++ README.md
@@ -1,1 +1,35 @@
-# image-resize
+
+# image conversion
+
+this project in built and delivered through udacity workspace, 
+I need to mention that working with this workspace is really hard to manage with but finally I finished it.
+
+
+
+## API Reference
+
+#### Get and resize any image that exist under images/full/ folder
+
+you can resize an image by supplying the image name and your resolution:
+
+```http
+  GET /api/images?filename={imageName}&width={number}&height={number}
+```
+
+
+After the resize occurs, the generated image will be stored under images/thumbnails folder and you will be able to see it too.
+
+
+## Scripts
+
+```http
+  npm run lint
+  npm run prettier
+  npm run build
+  npm run test
+  npm run jasmine
+  npm run start
+```
+
+
+    
---
+++ src/index.ts
@@ -0,0 +1,41 @@
+import express from 'express'; 
+import sharp from 'sharp';
+import path from 'path';
+import logger from './utilities/logger';
+import {promises as fsPromises} from 'fs';
+
+const app = express();
+const port = 3000;
+
+app.get('/api/images', logger, async(req, res) => {
+    const endFile = req.hostname.replace('-3000', '');
+    const width = req.query.width;
+    const height = req.query.height;
+    const inputFile = path.join(__dirname, `../images/full/${req.query.filename}.jpg`);
+    const outputFile = path.join(__dirname, `../images/thumbnails/${req.query.filename}-${width}x${height}.jpg`);
+    try {
+        if (!req.query.filename){
+            throw new Error('does not exist');
+        }
+        await sharp(inputFile) 
+            .resize(Number(width), Number(height))
+            .toFile(outputFile);
+        res.send(`<img src=https://${endFile}/files/images/thumbnails/${req.query.filename}-${width}x${height}.jpg>`);
+    } catch (err){
+        console.log('encountered an error');
+        res.send(`please add your filename and your desired resolution as this format: 
+                  path?filename=xxx&width=xxx&height=xxx`)
+        }
+});
+
+app.get('/', logger, (req, res) => {
+    res.send("please visit /api/images  with your all parameters set");
+});
+
+
+app.listen(port, () => {
+    console.log(`Listening on port ${port}!`);
+});
+
+
+export default app;
